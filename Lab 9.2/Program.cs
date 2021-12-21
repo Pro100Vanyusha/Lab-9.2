8edfba6db87a7e12026ededfe428b123899b4bea
Іван Музика <i.muzyka@kpnu.km.ua>
2021-12-21 16:22:53 +0200

    Добавьте файлы проекта.

---
+++ Lab 9.2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lab_9._2
+{
+        
+    public delegate void Fun(double a, double b, int n);
+    class TestMain
+    {
+        private static void Calc(double a, double b, int n)
+        {
+            static double f(double x) => x / (x - 1);
+            var h = (b - a) / n;
+            var sum = 0d;
+            for (var i = 1; i <= n; i++)
+            {
+                var x = a + i * h;
+                sum += f(x);
+            }
+
+            var res = h * sum;
+            Console.WriteLine($"Вiдповiдь: {Math.Round(res, 3)}");
+        }
+        private static void Input(double a, double b, int n)
+        {
+            Console.WriteLine($"Введенi значення: [{a}, {b}, {n}]");
+        }
+        public static void Main()
+        {
+            Fun x = new(Input);
+            Fun y = new(Calc);
+            x += y;
+            x(2.15, 4.99, 3);
+            x += x;
+            x(0.5, 3, 4);
+            x -= y; x -= y; x -= y;
+            x(1.25, 4.8, 7);
+            Console.WriteLine();
+        }
+    }
+}
